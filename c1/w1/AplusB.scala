2eea4306b1f862807b43c1d5d44621edb6b7dc91
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2018-03-02 17:55:19 +0100

    exercise 2 not finished

--- c1/w1/AplusB.scala
+++ c1/w1/AplusB.scala
@@ -1,7 +1,10 @@
 object AplusB {
   def main(args: Array[String]) {
-    val a=args(0).toInt
-    val b=args(1).toInt
-    println(a+b)
+    val input = scala.io.StdIn.readLine().split(" ")
+    if (input.size == 1 || input.size>3) {
+    	println("Abnormal size, exitting")
+    	System.exit(0)
+    }
+    println(input.map(x=>x.toInt).reduceLeft(_ + _))
   }
 }
