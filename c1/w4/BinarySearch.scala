48062005957a944b96ad525e6d516bf8f496f55b
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2018-03-28 15:40:58 +0200

    Binary search correct implementation completed

--- c1/w4/BinarySearch.scala
+++ c1/w4/BinarySearch.scala
@@ -1,19 +1,37 @@
-object LinearSearch {
+object BinarySearch {
   def main(args: Array[String]) {
     val input = scala.io.StdIn.readLine().split(" ").map(x=>x.toInt)
     val key= scala.io.StdIn.readInt()
 
-    print(linearSearchRec(input, 0, key))
+    // print(bSearchRec(input, 0, input.size, key))
+    print(bSearchIt(input, 0, input.size, key))
 
   }
 
-  def linearSearchRec(arr:Array[Int],start:Int, key:Int ):Int = {
-    if (start>=arr.size)  	return -1
-  	val elem=arr(start)
+  def bSearchRec(arr:Array[Int], start:Int, finish:Int, key:Int):Int = {
+    if (finish<start)  	return start-1
+    val mid = start+(finish-start)/2
+  	val elem=arr(mid)
   	if (elem==key) {
-  	 return start
-  	 } else if (elem<key) {
-  	 	linearSearchRec(arr, start+(arr.size - start)/2, key)
-  	 } else  return -1	
+  	 return mid
+  	 } else if (key<elem) {
+  	 	bSearchRec(arr, start, mid-1, key)
+  	 } else bSearchRec(arr, mid+1, finish, key)
+  }
+
+  def bSearchIt(arr:Array[Int], start:Int, finish:Int, key:Int):Int = {
+    var finishMut =finish
+    var startMut =start
+    while (startMut<=finishMut) {
+      var mid = startMut+(finishMut-startMut)/2
+      var elem=arr(mid)
+
+     if (elem==key) {
+      return mid
+     } else if (key<elem) {
+      finishMut=mid-1
+     } else startMut=mid+1 
+    }
+    return startMut - 1
   }
 }
