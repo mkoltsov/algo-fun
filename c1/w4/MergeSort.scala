c5f3fefd5698222408b14fd4ac7bcbd7bcf46ac8
mkoltsov <root@javabean.ru>
2018-04-05 00:02:01 +0200

    merge sort starts to work but only for a small data set

--- c1/w4/MergeSort.scala
+++ c1/w4/MergeSort.scala
@@ -3,7 +3,7 @@ object ScanSort {
 
   def main(args: Array[String]) {
     val input = scala.io.StdIn.readLine().split(" ").map(x=>x.toInt)
-    print(mergeSort(input).deep.mkString("\n"))
+    print(mergeSort(input).deep.mkString(" "))
   }
 
   def mergeSort(arr:Array[Int]): Array[Int] = {
@@ -23,7 +23,8 @@ def printArr(a:Array[Int]):String = a.deep.mkString(sep)
 
 def merge(arr1:Array[Int], arr2:Array[Int]): Array[Int] = {
   // var d = Array.fill[Int](arr1.size+arr2.size)(0)
-  var d = Array[Int](0)
+  println(s"input ${printArr(arr1)} - ${printArr(arr2)}")
+  var d = Array[Int]()
   while (arr1.filter(_ != -1).nonEmpty && arr2.filter(_ != -1).nonEmpty) {
     println(s"arrays: ${arr1.filter(_ != -1).nonEmpty} ${arr2.filter(_ != -1).nonEmpty} ${arr1.deep.mkString(sep)} - ${arr2.deep.mkString(sep)}")
     
@@ -33,21 +34,23 @@ def merge(arr1:Array[Int], arr2:Array[Int]): Array[Int] = {
     println(s"b=$b c=$c")
 
     if (b<=c) {
-      print(s"there - $b")
+      println(s"concat ${printArr(d:+b)}")
+      println(s"there - $b - ${printArr(d:+b)}")
       d = d :+ b
-      println(s"1 ${printArr(d)}")
+      println(s"d after concat ${printArr(d)}")
       arr1(0) = -1
     } else {
-      println(printArr(d:+c))
+      println(s"concat ${printArr(d:+c)}")
       println(s"here - $c - ${printArr(d:+c)}")
-      d = d:+c
+      d = d :+ c
       println(s"d after concat ${printArr(d)}")
       arr2(0) = -1
     }
   }
-  println(s"finally ${printArr(arr1.tail)} ${printArr(arr2.tail)}")
-  val ret = d ++ arr1.tail ++ arr2.tail
-  println(printArr(ret))
+  println(s"$arr1 - $arr2 - $d")
+  println(s"finally arr1 ${printArr(arr1.filter(_ != -1))} arr2 ${printArr(arr2.filter(_ != -1))} d ${printArr(d)}")
+  val ret = d ++ arr1.filter(_ != -1) ++ arr2.filter(_ != -1)
+  println(s"ret ${printArr(ret)}")
   ret 
 }
 }
