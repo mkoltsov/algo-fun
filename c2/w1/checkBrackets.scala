299a4729fa9ac5abb185010200c8afc5377a3af1
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2018-05-14 16:09:31 +0200

    first iteration

--- c2/w1/checkBrackets.scala
+++ c2/w1/checkBrackets.scala
@@ -1,36 +1,39 @@
+import scala.reflect.ClassTag
 object Brackets {
   val sep = " "
 
-  case class Stack() {
-    private var interal = Array[String]()
+  class Stack[A:ClassTag] {
+    private var interal:Array[A] = Array[A]()
 
-    def push(elem:String) = interal:+=elem
+    def push(elem:A) = interal:+=elem
+
     def pop() = {
       val tail = interal.last
       interal = interal.init
       tail
     }
 
-    override def toString():String=internal.deep.mkString(" ")
+    def empty() = interal.isEmpty
+
+    override def toString():String = interal.deep.mkString(" ")
   }
 
   def main(args: Array[String]) {
-    val value = scala.io.StdIn.readInt()
-    // val coins = scala.io.StdIn.readLine().split(" ").map(x=>x.toInt)
-    val minNumCoins=Array(0)
-
-    for (w <- 1 to value) {
-      minNumCoins(w) = 0
-      for (i <- Array(6,3,4,2)) {
-        if (m >= i) {
-          val numCoins = minNumCoins(m-i) +1 
-          if (numCoins < minNumCoins(m)) {
-            minNumCoins(m) = numCoins
-          }
-        }
-      }
-    }
-
-    print(minNumCoins(money))
+    val value = scala.io.StdIn.readLine()
+    print(isBalanced(value))
   } 
+
+  def isBalanced(str:String) = {
+      val stack = new Stack[Char]
+      for (char <- str) {
+      if (char == '(' || char == '[') {
+        stack.push(char)
+      } else {
+        if (stack.empty()) false
+        val top = stack.pop()
+        if ((top=='[' && char!=']') || (top=='(' && char!=')')) false
+      }
+      stack.empty()
+    }
+  }
 }
