1c59ec4c2898f4830b4437e9ad711d0e6a365731
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2018-06-05 01:42:05 +0200

    relieve

--- c2/w1/tree_height.java
+++ c2/w1/tree_height.java
@@ -2,6 +2,19 @@ import java.util.*;
 import java.io.*;
 
 public class tree_height {
+
+    class Node {
+    	List<Node> children = new ArrayList<Node>();
+    	void addChildren(Node elem) {
+    		children.add(elem);
+    	}
+
+    	@Override
+    	public String toString() {
+    		return children.toString();
+    	}
+    }
+
     class FastScanner {
 		StringTokenizer tok = new StringTokenizer("");
 		BufferedReader in;
@@ -22,28 +35,55 @@ public class tree_height {
 
 	public class TreeHeight {
 		int n;
-		int parent[];
+		List<Node> nodes = new ArrayList<Node>();
+		int root;
 		
 		void read() throws IOException {
 			FastScanner in = new FastScanner();
 			n = in.nextInt();
-			parent = new int[n];
+			int[] indexes = new int[n];
+			// nodes=new Node[n];
 			for (int i = 0; i < n; i++) {
-				parent[i] = in.nextInt();
+				int parentIndex=in.nextInt();
+				indexes[i] = parentIndex;					
+				nodes.add(i, new Node());
 			}
+			System.out.println(3);
+
+			for (int i = 0; i < n; i++) {
+				int parentIndex=indexes[i];
+				if (parentIndex == -1) {
+					root=i;
+				} else {
+					nodes.get(parentIndex).addChildren(nodes.get(i));
+				}
+			}
+		
+
+
+
 		}
 
 		int computeHeight() {
-                        // Replace this code with a faster implementation
-			int maxHeight = 0;
-			for (int vertex = 0; vertex < n; vertex++) {
-				int height = 0;
-				for (int i = vertex; i != -1; i = parent[i])
-					height++;
-				maxHeight = Math.max(maxHeight, height);
+				int max=0;
+			for (Node i : nodes) {
+				max=Math.max(i.children.size()+1, max);
+				// if (Math.max(i.children.size> max) {
+				// 	max=
+				// }
 			}
-			return maxHeight;
+			return max;	
 		}
+  //                       // Replace this code with a faster implementation
+		// 	int maxHeight = 0;
+		// 	for (int vertex = 0; vertex < n; vertex++) {
+		// 		int height = 0;
+		// 		for (int i = vertex; i != -1; i = parent[i])
+		// 			height++;
+		// 		maxHeight = Math.max(maxHeight, height);
+		// 	}
+		// 	return maxHeight;
+		// }
 	}
 
 	static public void main(String[] args) throws IOException {
@@ -59,6 +99,10 @@ public class tree_height {
 	public void run() throws IOException {
 		TreeHeight tree = new TreeHeight();
 		tree.read();
+		System.out.println("root " + tree.root);
+		// System.out.println("nodes "+ Arrays.toString(tree.nodes));
+		// tree.nodes.forEach(name -> System.out.println(name));
+		// System.out.println("nodes "+ Arrays.toString(tree.nodes));
 		System.out.println(tree.computeHeight());
 	}
 }
